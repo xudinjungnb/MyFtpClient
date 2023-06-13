33cc0d8183be518d366a9c1ae94edbefc939f312
xudinjungnb <1322936952@qq.com>
2023-06-13 17:20:38 +0800

    v1.0

---
+++ build/CMakeFiles/ftp.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/xdj/C语言/FtpClient/src/NetSocket.c" "/home/xdj/C语言/FtpClient/build/CMakeFiles/ftp.dir/src/NetSocket.c.o"
+  "/home/xdj/C语言/FtpClient/src/ftp_client.c" "/home/xdj/C语言/FtpClient/build/CMakeFiles/ftp.dir/src/ftp_client.c.o"
+  "/home/xdj/C语言/FtpClient/src/main.c" "/home/xdj/C语言/FtpClient/build/CMakeFiles/ftp.dir/src/main.c.o"
+  "/home/xdj/C语言/FtpClient/src/tools.c" "/home/xdj/C语言/FtpClient/build/CMakeFiles/ftp.dir/src/tools.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ftp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ftp.dir/src/main.c.o"
+  "CMakeFiles/ftp.dir/src/NetSocket.c.o"
+  "CMakeFiles/ftp.dir/src/ftp_client.c.o"
+  "CMakeFiles/ftp.dir/src/tools.c.o"
+  "../bin/ftp.pdb"
+  "../bin/ftp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ftp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
